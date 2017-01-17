036faa34ab01b95d3c32642183a2f379f7b6b612
admin <admin@DESKTOP-PG7NG1E>
2017-01-17 16:36:22 +0800

    Add project files.

---
+++ Jaiden.Proof/OddEven.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jaiden.Proof
+{
+    public class OddEven
+    {
+        public void ExistNumber(int num)
+        {
+            if((num & 1) > 0){
+                Console.WriteLine("奇数");
+            }
+            else
+            {
+                Console.WriteLine("偶数");
+            }
+
+        }
+
+    }
+}
---
+++ Jaiden.Proof/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jaiden.Proof
+{
+    class NDateTime
+    {
+        private DateTime value;
+
+        public static implicit operator DateTime(NDateTime value)
+        {
+            return value.value;
+        }
+
+        public static implicit operator NDateTime(DateTime value)
+        {
+            return new NDateTime() { value = value };
+        }
+
+        public static implicit operator string(NDateTime value)
+        {
+            return value.value.ToString();
+        }
+
+        public static NDateTime Now
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        public static explicit operator long(NDateTime value)
+        {
+            return value.value.Ticks;
+        }
+ 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string value = NDateTime.Now;
+            DateTime dt = NDateTime.Now;
+
+            long c = (long)NDateTime.Now;
+
+            Console.WriteLine(value);
+
+            Console.WriteLine(c);
+
+            //OddEven e = new OddEven();
+            //e.ExistNumber(23238);
+            Console.Read();
+        }
+    }
+}
