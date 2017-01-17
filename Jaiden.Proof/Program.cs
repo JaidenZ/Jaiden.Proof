4a2750b4f91746e7c17bf12ac0ddcdd7bc64c987
admin <admin@DESKTOP-PG7NG1E>
2017-01-17 17:01:13 +0800

    commit implicit exam

--- Jaiden.Proof/Program.cs
+++ Jaiden.Proof/Program.cs
@@ -5,57 +5,18 @@ using System.Text;
 
 namespace Jaiden.Proof
 {
-    class NDateTime
-    {
-        private DateTime value;
-
-        public static implicit operator DateTime(NDateTime value)
-        {
-            return value.value;
-        }
-
-        public static implicit operator NDateTime(DateTime value)
-        {
-            return new NDateTime() { value = value };
-        }
-
-        public static implicit operator string(NDateTime value)
-        {
-            return value.value.ToString();
-        }
-
-        public static NDateTime Now
-        {
-            get
-            {
-                return DateTime.Now;
-            }
-        }
-
-        public static explicit operator long(NDateTime value)
-        {
-            return value.value.Ticks;
-        }
- 
-    }
-
+    
     class Program
     {
         static void Main(string[] args)
         {
 
 
-            string value = NDateTime.Now;
-            DateTime dt = NDateTime.Now;
-
-            long c = (long)NDateTime.Now;
-
+            string value = IDateTime.Now;
+            DateTime dt = IDateTime.Now;
+            
             Console.WriteLine(value);
-
-            Console.WriteLine(c);
-
-            //OddEven e = new OddEven();
-            //e.ExistNumber(23238);
+            
             Console.Read();
         }
     }
---
+++ Jaiden.Proof/implicitEntity/IDateTime.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jaiden.Proof
+{
+
+    public partial class IDateTime
+    {
+        private DateTime _this;
+
+        #region structure
+        
+        public static implicit operator DateTime(IDateTime value)
+        {
+            return value._this;
+        }
+        public static implicit operator IDateTime(DateTime value)
+        {
+            return new IDateTime() { _this = value };
+        }
+        #endregion
+
+        #region expression
+
+
+        public static implicit operator string(IDateTime value)
+        {
+            return value._this.ToString("yyyy MM dd HH:mm:ss");
+        }
+
+        public static implicit operator IDateTime(string value)
+        {
+            IDateTime date = new IDateTime();
+            date._this = DateTime.Now;
+
+            if(string.IsNullOrEmpty(value)){
+                try
+                {
+                    date._this = Convert.ToDateTime(value);
+                }
+                catch { };
+            }
+            return date;
+        }
+
+        #endregion
+
+        public static IDateTime Now
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+    }
+}
