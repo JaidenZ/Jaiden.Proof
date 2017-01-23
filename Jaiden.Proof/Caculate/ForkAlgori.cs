d45be49d2a0bb07eb50afbecd3af3e45d405ab99
JaidenZ <Haojun.Zhao@icloud.com>
2017-01-23 14:42:33 +0800

    Add Forkgori

--- Jaiden.Proof/Caculate/ForkAlgori.cs
+++ Jaiden.Proof/Caculate/ForkAlgori.cs
@@ -30,9 +30,31 @@ namespace Jaiden.Proof
                 //判断数据
                 Console.WriteLine(left + "-" + right);
             }
-
         }
 
+        public static void OForkgori(List<object> array)
+        {
+            int arraylen = array.Count;
+            int mid = arraylen / 2;
+            //获取中间位置
+            if ((arraylen-- & 1) > 0)
+            {
+                object mido = array[mid];
+
+                //判断数据
+                Console.WriteLine("first:" + mido);
+            }
+            //从中间向两边检索
+            for (int i = mid; i > 0; i--)
+            {
+                int j = mid + i;
+                object left = array[i];
+                object right = array[j];
+
+                //判断数据
+                Console.WriteLine(left + "-" + right);
+            }
+        }
 
     }
 }
